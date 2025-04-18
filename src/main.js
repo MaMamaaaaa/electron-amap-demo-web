63367798e0be9dbbb6b89bdf530e226b8316f415
masirui <3236817895@qq.com>
2025-04-18 22:00:13 +0800

    Initial commit with basic AMap integration

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile('src/index.html')
+
+    // 开发时打开开发者工具
+    // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
